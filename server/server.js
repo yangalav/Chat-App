bec1a849fc4b50de6faedcca14c7dac40d440968
Viswada Yangala <yangalav@go.stockton.edu>
2017-07-10 21:43:05 -0400

    listening on connection event for incoming sockets

--- server/server.js
+++ server/server.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const io = require('socket.io');
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
 app.use(express.static(path.join(__dirname, '../client/')));
 require('./routes.js')(app, express);
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+});
 
 var port = process.env.PORT || 8080;
 
